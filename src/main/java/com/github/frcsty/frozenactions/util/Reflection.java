9f71d27fe811dae86af42385b5a8f98581bbebf2
Frcsty <frostyprime13@gmail.com>
2021-10-16 20:04:42 +0200

    some fixes

--- src/main/java/com/github/frcsty/frozenactions/util/Reflection.java
+++ src/main/java/com/github/frcsty/frozenactions/util/Reflection.java
@@ -4,14 +4,11 @@ import org.bukkit.Bukkit;
 
 public final class Reflection {
 
-    static final String VERSION = Bukkit.getServer().getClass().getPackage().getName().split(".")[3];
+    private static final String[] VERSION_COMPONENTS = Bukkit.getServer().getClass().getPackage().getName().split("\\.");
+    static final String VERSION = VERSION_COMPONENTS.length >= 4 ? VERSION_COMPONENTS[3] : "Not 1.8 Woohoo!";
 
     public static boolean is1_8() {
         return VERSION.contains("v1_8");
-    }
-
-    public static boolean is1_16() {
-        return VERSION.contains("v1_16");
     }
 
     public static Class<?> getNMSClass(final String className) {
